238cc8f75b934a4e76bec22b521fc59f4ad397aa
Lekha <lekhavc11@gmail.com>
2021-03-15 01:33:54 -0700

    completed

--- DESIGN/Design Basics/Implement Queue using Stacks.py
+++ DESIGN/Design Basics/Implement Queue using Stacks.py
@@ -1,10 +1,41 @@
+class queueUsingStack:
+    def __init__(self):
+        self.inQueue = []
+        self.outQueue = []
+        
+    def push(self, x):
+        self.inQueue.append(x)
+        
+    def fillOutQueue(self):
+        l = len(self.inQueue)
+        while l > 0:
+            self.outQueue.append(self.inQueue.pop())
+            l -= 1
+
+    def pop(self):
+        if len(self.outQueue) == 0:
+            self.fillOutQueue()
+        return self.outQueue.pop() if len(self.outQueue) != 0 else -1
+
+    def peek(self):
+        if len(self.outQueue) == 0:
+            self.fillOutQueue()
+        return self.outQueue[-1] if len(self.outQueue) != 0 else -1
+            
+    def empty(self):
+        if len(self.inQueue) == 0 and len(self.outQueue) == 0:
+            return True
+        return False
+
+
+# ————————————————————————————————————————————————————————————————————————————————————————————————
+
+
 class MyQueue:                         #Queue is FIFO
-    
     def __init__(self):
         self.s1 = []
         self.s2 = []
         
-
     def push(self, x):              
         while len(self.s1) != 0:        #Emptying the stack1 to stack2
             self.s2.append(self.s1.pop())  
@@ -28,31 +59,3 @@ class MyQueue:                         #Queue is FIFO
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
-
-
-
-# ————————————————————————————————————————————————————————————————————————————————————————————————
-class queueUsingStack:
-    def __init__(self):
-        self.inqueue = []
-        self.outqueue = []
-
-    def push(self, x):
-        self.inqueue.append(x)
-
-    def pop(self):
-        if self.outqueue != []:
-            return self.outqueue.pop()
-        while self.inqueue != []:
-            self.outqueue.append(self.inqueue.pop())
-        return self.outqueue.pop() if self.outqueue != [] else -1
-            
-    def peek(self):
-        if self.outqueue != []:
-            return self.outqueue[-1]
-        while self.inqueue != []:
-            self.outqueue.append(self.inqueue.pop())
-        return self.outqueue[-1]
-        
-    def empty(self):
-        return self.inqueue==[] and self.outqueue==[]
